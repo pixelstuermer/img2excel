bb9976c0d12a3604e1d885b98a1a8b69667d4997
Philipp Montesano <philipp.montesano@icloud.com>
2018-01-10 16:55:56 +0100

    Improved builds

---
+++ img2excel-cli/src/main/java/com/github/pixelstuermer/img2excel/cli/application/Img2ExcelApplicationStarter.java
@@ -0,0 +1,9 @@
+package com.github.pixelstuermer.img2excel.cli.application;
+
+public class Img2ExcelApplicationStarter {
+
+   public static void main( String[] args ) {
+      System.out.println( "Here I am!" );
+   }
+
+}
