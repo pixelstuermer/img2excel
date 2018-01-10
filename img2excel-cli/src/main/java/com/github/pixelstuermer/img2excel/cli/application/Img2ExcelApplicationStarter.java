90625452ede80f9cc75c341da46e8b61a7b00229
Philipp Montesano <philipp.montesano@icloud.com>
2018-01-10 18:20:40 +0100

    Added properties to POM for properties file

--- img2excel-cli/src/main/java/com/github/pixelstuermer/img2excel/cli/application/Img2ExcelApplicationStarter.java
+++ img2excel-cli/src/main/java/com/github/pixelstuermer/img2excel/cli/application/Img2ExcelApplicationStarter.java
@@ -9,12 +9,15 @@ import com.github.pixelstuermer.img2excel.core.converter.Img2ExcelConverter;
 public class Img2ExcelApplicationStarter {
 
    public static void main( String[] args ) throws IOException {
-      java.io.InputStream is = Img2ExcelApplicationStarter.class.getResourceAsStream( "my.properties" );
+      java.io.InputStream is = Img2ExcelApplicationStarter.class.getResourceAsStream( "img2excel.properties" );
       java.util.Properties p = new Properties();
       p.load( is );
-      String version = p.getProperty( "version" );
 
-      System.out.println( version );
+      System.out.println( p.getProperty( "name" ) );
+      System.out.println( p.getProperty( "version" ) );
+      System.out.println( p.getProperty( "author" ) );
+      System.out.println( p.getProperty( "repository" ) );
+
       Img2ExcelConverter img2ExcelConverter = new Img2ExcelConverter( new File( args[0] ) );
       img2ExcelConverter.convertImageToExcel();
    }
