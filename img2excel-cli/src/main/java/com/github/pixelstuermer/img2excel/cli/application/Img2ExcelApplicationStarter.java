bb583d228c0190fdf58d445bf2e4fdd9f723dbe7
Philipp Montesano <philipp.montesano@icloud.com>
2018-01-10 17:57:58 +0100

    Tried to integrate properties

--- img2excel-cli/src/main/java/com/github/pixelstuermer/img2excel/cli/application/Img2ExcelApplicationStarter.java
+++ img2excel-cli/src/main/java/com/github/pixelstuermer/img2excel/cli/application/Img2ExcelApplicationStarter.java
@@ -1,9 +1,21 @@
 package com.github.pixelstuermer.img2excel.cli.application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.github.pixelstuermer.img2excel.core.converter.Img2ExcelConverter;
+
 public class Img2ExcelApplicationStarter {
 
-   public static void main( String[] args ) {
-      System.out.println( "Here I am!" );
+   public static void main( String[] args ) throws IOException {
+      java.io.InputStream is = Img2ExcelApplicationStarter.class.getResourceAsStream( "my.properties" );
+      java.util.Properties p = new Properties();
+      p.load( is );
+      String version = p.getProperty( "version" );
+      System.out.println( version );
+      Img2ExcelConverter img2ExcelConverter = new Img2ExcelConverter( new File( args[0] ) );
+      img2ExcelConverter.convertImageToExcel();
    }
 
 }
